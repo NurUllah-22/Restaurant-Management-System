3959194e24e196eba469dc4c43dd5d0fa591b18f
MohammadMuntasirKabir <mohammad.muntasir.kabir@gmail.com>
2025-05-17 13:26:43 +0600

    Added remaining project via upload - 1

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	@include('home.css')
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
+    @include('home.header')
+    @include('home.about')
+    @include('home.gallery')
+    @include('home.table')
+    @include('home.blog')
+    @include('home.reviews')
+    @include('home.contact')
+    @include('home.footer')
+</body>
+</html>
